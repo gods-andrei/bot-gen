d6fe07c0c87587f5e2743ce86099d2bf21d2c4c5
gods-andrei <60801468+gods-andrei@users.noreply.github.com>
2020-02-07 16:55:53 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# bot-gen
-host on heroku
+# AccountGenBot
+💙 Discord bot that generates account from a text file!
+## This bot violates Discord's ToS! USE IT AT YOUR OWN RISK!
---
+++ gen.js
@@ -0,0 +1,58 @@
+// Account generator bot for Discord by jewdev (fromm Nulled.TO)
+const Discord = require('discord.js');
+const client = new Discord.Client({disableEveryone: true});
+
+const config = require('./config.json');
+
+const chalk = require('chalk');
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+fs.readdir('./commands/', (err, files) => {
+    if(err) throw err;
+
+    let file = files.filter(f => f.endsWith('.js'));
+    if(file.length <= 0) return console.log('There is js files in the commands folder');
+
+    file.forEach((f) => {
+        let props = require(`./commands/${f}`);
+        console.log(chalk.yellow(`Attempting to load ${f}`));
+
+        client.commands.set(props.help.name, props);
+    });
+    console.log(chalk.bold.bgGreen('Everything loaded properly.'));
+});
+
+process.on('unhandledRejection', error => {
+    console.error('Unhandled promise rejection:', error);
+});
+
+client.on('error', () => console.error);
+
+client.on('warn', () => console.warn);
+
+client.on('ready', async () => {
+    console.log(`This bot made by ${chalk.cyan('gods andrei')} from ${chalk.bold.bgBlue('what')}!`);
+
+    client.user.setActivity('Quick GEN', {
+        type: 'WATCHING',
+        url: 'youtube.com/AndreiIMT'
+    });
+});
+
+client.on('message', async (msg) => {
+    if(msg.author.bot) return;
+    if(!msg.content.startsWith(config.PREFIX)) return;
+    if(msg.content.indexOf(config.PREFIX) != 0) return;
+    if(msg.channel.type == 'dm') return;
+
+    const args = msg.content.slice(config.PREFIX.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, msg, args, config);
+});
+
+client.login(config.TOKEN);
